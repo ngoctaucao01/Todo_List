1528876f8e9901fec6eb22b767b84aa2716ce96d
Cao Ngoc Tau <tau.cao21@pn.com.vn>
2020-07-24 08:46:00 +0700

    todo list

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoItem from './components/TodoItem';
+import tick from './img/tick.svg';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state= {  
+      newItem:'',
+      todoItems: [
+        {title: 'Buy Candy', isComplete: true},
+        {title: 'Play Soccer', isComplete: true},
+        {title: 'Rent House'}
+      ]
+    }
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onItemClicked(item){
+    return (event)=>{
+      const isComplete = item.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,      
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index+1)
+        ]   
+      });
+    };
+  }
+
+  onKeyUp(event){
+    if(event.keyCode === 13){ //enter key
+      let text = event.target.value;
+      if(!text){
+        return;
+      }
+
+      text = text.trim();
+      if(!text){return;}
+
+      this.setState({
+        todoItems:[
+          {title: text, isComplete:false},
+          ...this.state.todoItems,
+        ]
+      });
+    } 
+    
+  }
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    });
+  }
+  render() {
+    const { todoItems, newItem } = this.state;
+    if(todoItems.length){
+      return (
+        <div className="App"> 
+          <div className="Header">
+            <img src={tick} width={32} height={32} />
+            <input type="text" placeholder="Add a new item" value={newItem} onChange={this.onChange} onKeyUp={this.onKeyUp}/>
+          </div>
+          {todoItems.length && todoItems.map((item, index) => 
+              <TodoItem 
+                key={index} 
+                item={item} 
+                onClick={this.onItemClicked(item)} />
+            ) 
+          }
+        </div>
+      );}
+  }
+}
+
+export default App;
